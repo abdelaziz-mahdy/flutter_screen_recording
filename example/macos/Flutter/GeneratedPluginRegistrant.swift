b244dba0e2fbc7eaa1a9d886af76a28ca0ebbd91
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-29 12:10:57 +0300

    macos support

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import shared_preferences_macos
+import flutter_screen_recording
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterScreenRecordingPlugin.register(with: registry.registrar(forPlugin: "FlutterScreenRecordingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
